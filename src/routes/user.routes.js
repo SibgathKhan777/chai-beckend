4664c12c9d7509502fa2444667ef6a8e3e87f346
SibgathKhan777 <novamsk800@gmail.com>
2025-03-16 23:07:56 +0530

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js"; // Added `.js`
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser); // Fixed typo
+router.route("/register").post(
+    upload.fields(
+        [
+            {name:"avatar",
+                maxCount:1
+            },
+            {
+                name:"coverImage",
+                maxCount:1
+            }
+        ]
+    ),
+    registerUser); // Fixed typo
 
 export default router;
